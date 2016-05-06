1cfe3cc6b52c3f3d0516d924298b8e6e54097f4a
Max Lebedev <truesilv@gmail.com>
2016-05-06 18:52:29 +0500

    v 0.0.2

--- app/app.js
+++ app/app.js
@@ -2,10 +2,24 @@
 
 angular.module('myApp', ['rbTest.mailbox'])
     .controller('MyAppBaseCtrl', ['$scope', function ($scope) {
+
+        function getRandomEmail (nameLength, domainLength, zone) {
+            var zones = ['com', 'org', 'ru'],
+                startCh = 'qwertyuiopasdfghjklzxcvbnm',
+                randomName = (+new Date() * Math.random()).toString(36).substring(0, (nameLength || 5) - 1),
+                randomDomain = (+new Date() * Math.random()).toString(36).substring(0, (domainLength || 5) - 1);
+
+            randomName = startCh.charAt(Math.floor(Math.random() * startCh.length)) + randomName;
+            randomDomain = startCh.charAt(Math.floor(Math.random() * startCh.length)) + randomDomain;
+
+            return randomName + '@' + randomDomain + '.' + (zone || zones[Math.floor(Math.random() * zones.length)]);
+        }
+
       $scope.addEmails = function () {
         var ctrl = angular.element(document.querySelector('#ee1')).controller('emailEditor');
-        ctrl.makeBlocks('test');
+        ctrl.makeBlocks(getRandomEmail());
       };
+
       $scope.getEmailsCount = function () {
         var ctrl = angular.element(document.querySelector('#ee1')).controller('emailEditor');
         alert(ctrl.getMailsCount());
--- app/emailEditor/emailEditor.js
+++ app/emailEditor/emailEditor.js
@@ -48,6 +48,10 @@ angular.module('rbTest.mailbox', [])
           $scope.mails.splice(index, 1);
         };
 
+        $scope.isEmail = function (value) {
+            return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(value);
+        };
+
         this.makeBlocks = makeBlocks;
         this.getMailsCount = function () {
           return $scope.mails.length;
